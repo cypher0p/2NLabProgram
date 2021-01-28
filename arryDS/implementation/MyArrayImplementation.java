bae2c28defb9ba1e99518338f13ac9c954cce855
Prajjwal <prajjwal199275@gmail.com>
2021-01-28 11:21:34 +0530

    Create an DS program

---
+++ arryDS/implementation/MyArrayImplementation.java
@@ -0,0 +1,39 @@
+package arryDS.implementation;
+
+import arryDS.myinterface.MyArray;
+
+public class MyArrayImplementation
+            implements MyArray {
+    private int[] arr;
+    private int index;
+
+    public int[] getArr() {
+        return arr;
+    }
+
+    public void setArr(int[] arr) {
+        this.arr = arr;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public MyArrayImplementation(int size) {
+        arr = new int[size];
+        index = 0;
+    }
+
+    @Override
+    public void traverse() {
+        System.out.println("printing elements");
+        for (int i = 0; i < index; i++) {
+            System.out.println(arr[i] + ", ");
+        }
+        System.out.println();
+    }
+}
