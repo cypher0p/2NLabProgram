4cee23a15db92b31e3edf971886eddf90b0d5e5e
Prajjwal <prajjwal199275@gmail.com>
2021-01-28 11:52:39 +0530

    modified MyMain.java code

--- arryDS/main/MyMain.java
+++ arryDS/main/MyMain.java
@@ -8,7 +8,9 @@ public class MyMain {
         MyArrayImplementation numbers = new MyArrayImplementation(size);
         System.out.println("insert = " + numbers.insert(2));
         System.out.println("insert = " + numbers.insert(22));
-        System.out.println("insert = " + numbers.insert(8));
+        System.out.println("insert = " + numbers.insert(7));
+        System.out.println("insert = " + numbers.insert(6));
+        System.out.println("insert = " + numbers.insert(5));
 
         numbers.traverse();
         numbers.delete();
@@ -16,6 +18,7 @@ public class MyMain {
         System.out.println("sorted:");
         numbers.sort();
         numbers.traverse();
-        System.out.println("Found = " + numbers.search(55));
+        System.out.println("Searching 22 in array");
+        System.out.println("Found = " + numbers.search(22));
     }
 }
